918751ebbbc4fd81cddef1a9f2383d0ffa532de6
AllenChang <111629702+BlueBoy247@users.noreply.github.com>
2025-07-16 00:25:00 +0800

    chore: adjust CSS for future development

--- src/App.vue
+++ src/App.vue
@@ -25,7 +25,10 @@ import AppNavBar from './components/AppNavBar.vue';
 <style scoped>
 header {
   line-height: 1.5;
+  min-height: 10vh;
   max-height: 100vh;
+  margin: 0 auto;
+  padding: 2rem;
 }
 
 .logo {
@@ -40,23 +43,11 @@ nav {
   margin-top: 2rem;
 }
 
+main {
+  min-height: 80vh;
+  margin: 0 auto;
+  padding: 2rem;
 }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
 }
 </style>
