b35c553cdc5a75d075f9a209f357fda9c047c804
AllenChang <111629702+BlueBoy247@users.noreply.github.com>
2025-07-17 23:16:22 +0800

    feat: add components to setting page
    Integrate GeneralSetting and NoteBoard into SettingView

--- src/views/SettingView.vue
+++ src/views/SettingView.vue
@@ -1,15 +1,27 @@
+<script setup>
+import GeneralSetting from '@/components/GeneralSetting.vue';
+import NoteBoard from '@/components/NoteBoard.vue';
+</script>
+
 <template>
-  <div class="title">
-    <h1>系統管理頁面</h1>
+  <div id="setting-board">
+    <GeneralSetting />
+    <hr>
+    <NoteBoard />
   </div>
 </template>
 
 <style scoped>
-@media (min-width: 1024px) {
-  .title {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
+#setting-board {
+  width: 100%;
+  border: 1px solid #DFE1E6;
+  border-radius: 14px;
+  background-color: #fff;
+  padding: 2rem;
+}
+
+hr {
+  margin: 1rem 0;
+  border: 1.5px solid #DFE1E6;
 }
 </style>
