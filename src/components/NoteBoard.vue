1ec50fbfa5df1750556107a7f12b5e7fa1b8d7b4
AllenChang <111629702+BlueBoy247@users.noreply.github.com>
2025-07-18 16:37:22 +0800

    refactor: 將搜尋圖示拆成獨立元件

--- src/components/NoteBoard.vue
+++ src/components/NoteBoard.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { ref, computed } from 'vue'
+import IconSearch from './icons/IconSearch.vue';
 
 const searchValue = ref('');
 
@@ -33,9 +34,7 @@ const filteredStudents = computed(() => {
       v-model="searchValue"
     />
     <label for="search">
-      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
-        <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0"/>
-      </svg>
+      <IconSearch />
     </label>
   </div>
 
