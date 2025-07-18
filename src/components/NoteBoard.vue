a062e4542a16e56e86ab818b3b549ae118f608e5
AllenChang <111629702+BlueBoy247@users.noreply.github.com>
2025-07-18 16:44:48 +0800

    refactor: 重構篩選機制並增加名單排序

--- src/components/NoteBoard.vue
+++ src/components/NoteBoard.vue
@@ -5,22 +5,47 @@ import IconSearch from './icons/IconSearch.vue';
 const searchValue = ref('');
 
 // sample data
-const studentsList = [
+const studentsList = ref([
   { id: 'U11016099', name: '學生一', note: '住宿生註記' },
   { id: 'U11116099', name: '學生二', note: null },
   { id: 'U11216099', name: '學生三', note: '違規註記' },
-  { id: 'U11316099', name: '學生四字', note: null },
-  { id: 'U11416099', name: 'Student Five Words', note: null }
-];
+  { id: 'U11316099', name: '學生四', note: null },
+  { id: 'U11416099', name: 'Student Five', note: null },
+  { id: 'G11016099', name: '學生六', note: '住宿生註記' },
+  { id: 'G11116099', name: '學生七', note: null },
+  { id: 'G11216099', name: '學生八', note: '違規註記' },
+  { id: 'G11316099', name: '學生九', note: null },
+  { id: 'G11416099', name: '學生十', note: null },
+  { id: 'M11016099', name: '學生十一', note: '住宿生註記' },
+  { id: 'M11116099', name: '學生十二', note: null },
+  { id: 'M11216099', name: 'Student Thirteen', note: null },
+  { id: 'M11316099', name: '學生十四', note: null },
+  { id: 'M11416099', name: '學生十五', note: null },
+  { id: 'U11316100', name: '學生十六', note: '住宿生註記' },
+  { id: 'U11316101', name: '學生十七', note: '違規註記' },
+  { id: 'U11316102', name: '學生十八', note: '住宿生註記' },
+  { id: 'U11316103', name: '學生十九', note: null },
+  { id: 'U11316104', name: '學生二十', note: null },
+  { id: 'U11316105', name: 'Student Twenty First', note: null },
+  { id: 'U11316106', name: 'Student Twenty Second', note: null },
+  { id: 'U11316107', name: 'Student Twenty Third', note: '違規註記' },
+  { id: 'U11316108', name: 'Student Twenty Fourth', note: null },
+  { id: 'U11316109', name: 'Student Twenty Fifth', note: null },
+]);
 
 const filteredStudents = computed(() => {
-  console.log(searchValue.value);
-  if (!searchValue.value.trim())
-    return studentsList;
-  return studentsList.filter(student =>
-    student.name.includes(searchValue.value) ||
-    student.id.includes(searchValue.value)
-  );
+  return studentsList.value.filter(student =>
+      !searchValue.value.trim() ||
+      student.id.includes(searchValue.value) ||
+      student.name.includes(searchValue.value)
+    ).sort((studentA, studentB) => {
+    const code = ['U', 'G', 'M'];
+    if (code.indexOf(studentA.id.charAt(0)) < code.indexOf(studentB.id.charAt(0))) return -1;
+    if (code.indexOf(studentA.id.charAt(0)) > code.indexOf(studentB.id.charAt(0))) return 1;
+    if (studentA.id.substring(1) < studentB.id.substring(1)) return -1;
+    if (studentA.id.substring(1) > studentB.id.substring(1)) return 1;
+    return 0;
+  });
 });
 </script>
 
