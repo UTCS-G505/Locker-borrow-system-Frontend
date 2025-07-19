5e0dd3bf306608a6fc0c196fe18045096c04e6e1
ShowMaker <a0979810349@gmail.com>
2025-07-19 21:04:48 +0800

    feat: 新增AppNavBar

--- src/App.vue
+++ src/App.vue
@@ -7,12 +7,6 @@ import AppFooter from './components/AppFooter.vue'
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-
     <nav>
       <AppNavBar />
     </nav>
