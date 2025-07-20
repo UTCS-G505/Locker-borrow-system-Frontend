5f8f677c78475542f94b66dc652a024cfbff3564
Doreen-123 <chenjiayun451@gmail.com>
2025-07-21 00:13:01 +0800

    fix: 加入註解與合併部分CSS的程式碼(原有一些CSS給的屬性太多餘，將其縮減)

--- src/components/Record_table.vue
+++ src/components/Record_table.vue
@@ -1,4 +1,8 @@
 <script setup>
+/* 
+    下面有個"empty-row" 作用是第三筆資料輸出後，下面留的空白處；
+    而" colspan="8" "作用是要橫跨 8 欄位的寬度
+*/
 const record = [
     {name:'陳胤華', type:'學年借用', start_time:'2024/9/1', end_time:'2025/6/30', num:'39', state:'審核中'},
     {name:'陳胤華', type:'學年借用', start_time:'2024/9/1', end_time:'2025/6/30', num:'39', state:'駁回'},
@@ -30,7 +34,7 @@ const record = [
                     <td>{{item.end_time}}</td>
                     <td>{{item.num}}</td>
                     <td>
-                        <button class="operate_button">詳細資料</button>
+                        <button class="operate_button">詳細資訊</button>
                     </td>
                     <td>{{item.state}}</td>
                     <td>
@@ -49,50 +53,36 @@ const record = [
 
 <style>
 
+/*
+    table那邊外層包了兩個<div>，我有嘗試過包一層會沒效果，包兩層才有，
+    然後CSS設定在第二層，這部分我有先跟Allen說過，那邊有點玄(?)，歡迎電神們的討論~~
+*/
+
 .inside_table{
+    position: relative;
     border-radius: 25px; 
-    overflow: hidden;
+    overflow: hidden; /* 跟 border-radius 做搭配，讓內容不要超出圓角邊框 */
     width: 105%;
     padding: 10px;
     margin: 20px auto;
     background-color: white;
-    position: relative;
 }
 
 table{
     border-radius: 25px;
-    border:4px solid #ECE8E8; 
-    border-collapse: separate; 
-    border-spacing: 0;
     overflow: hidden;
+    border:4px solid #ECE8E8; 
+    border-collapse: separate; /* 如果用collapse，圓角會被吃掉 */
+    border-spacing: 0;
     width: 97%;
 }
 
-tbody tr:last-child td {
-  padding-bottom: 50px; /* 你想要的空白高度 */
-}
-
-td,th{
-    border: none;
-    padding: 8px 12px;
-
-}
-
-tr th{
-    padding: 4px;
-}
-
-tr td{
-    text-align: center;
-    padding: 4px;
-}
-
 .head{
-    background-color: aliceblue
+    background-color: aliceblue;
 }
 
-tbody td{
-    border-bottom: 2px solid #ECE8E8;
+.head th{
+    border-bottom:2px solid #ECE8E8;
 }
 
 #data{
@@ -101,25 +91,32 @@ tbody td{
     border-spacing: 0px;
 }
 
-.bottom_space{
-    height: 200px;
+tr th{
+    padding: 4px;
+}
+
+tr td{
+    text-align: center; /* 字體置中 */
+    padding: 4px;
+    font-size: 17px;
+    color: black;
+    border-bottom: 2px solid #ECE8E8; /* 每一筆資料結束後，下面加入線條使每一筆資料分隔 */
+}
+
+td,th{
+    border: none; /* 不要有預設的格線 */
+    padding: 8px 12px; /* 上下8px，左右12px */
 }
 
 .operate_button{
     border: none;
-    border-color:black;
     background-color: #ECE8E8;
-    border-radius: 20px;
-    padding: 6px;
-}
-
-td{
-    font-size: 16px;
-    color: black;
+    border-radius: 10px;
+    padding: 2px 8px;
 }
 
 .empty-row td {
-  height: 200px; 
+  height: 200px; /* 製作下方空白處 */
   border: none;
   background: transparent;
 }
--- src/views/RecordView.vue
+++ src/views/RecordView.vue
@@ -9,52 +9,7 @@ import Record_table from '../components/Record_table.vue';
 </template>
 
 <style scoped>
-.text{
-  font-size: 35px;
-  color: black;
-  position: absolute;
-  top: 16px;
-  left: 580px;
-}
-.navbar span{
-  color: black;
-  font-weight: bold;
-  font-size: 23px;
-}
-.triangle-down{
-  width: 0;
-  height: 0;
-  margin-top: 12px;
-  font-size: 23px;
-  border-left: 5px solid transparent;
-  border-right: 5px solid transparent;
-  border-top: 6px solid black;
-  top: 10px;
-  cursor: pointer;
-}
-#first_text{
-  padding-left: 120px;
-}
-#last_text{
-  display: inline-flex;
-  gap: 4px;
-  color: black;
-  font-weight: bold;
-  padding-right: 50px;
-  font-size: 20px;
-  cursor: pointer; /*讓滑鼠變手指 使用者體驗更佳*/
-}
-.navbar{
-  position: absolute;
-  top: 110px;
-  left: 0%;
-  width: 100vw;
-  display: flex;
-  padding:10px 0;
-  justify-content: space-between;
-  background-color: aliceblue;
-  gap: 10px;
-}
+
 .record{
   font-size: 30px;
   color: black;
