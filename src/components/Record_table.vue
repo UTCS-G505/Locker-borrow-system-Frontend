f34a0e849dd9d68dc46fd5f0a59b5b83d84b04aa
Doreen-123 <chenjiayun451@gmail.com>
2025-07-20 21:40:38 +0800

    feat: 新增申請紀錄頁面

---
+++ src/components/Record_table.vue
@@ -0,0 +1,127 @@
+<script setup>
+const record = [
+    {name:'陳胤華', type:'學年借用', start_time:'2024/9/1', end_time:'2025/6/30', num:'39', state:'審核中'},
+    {name:'陳胤華', type:'學年借用', start_time:'2024/9/1', end_time:'2025/6/30', num:'39', state:'駁回'},
+    {name:'陳胤華', type:'臨時借用', start_time:'2024/9/1', end_time:'2025/6/30', num:'39', state:'借用中'}
+]
+</script>
+
+<template>
+    <div class="all_table">
+        <div class="inside_table">
+        <table>
+            <thead class="head">
+                <tr id="data">
+                    <th>申請人</th>
+                    <th>借用類型</th>
+                    <th>開始時間</th>
+                    <th>結束時間</th>
+                    <th>系櫃編號</th>
+                    <th>詳細資訊</th>
+                    <th>狀態</th>
+                    <th>操作</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="(item,index) in record" :key="index">
+                    <td>{{item.name}}</td>
+                    <td>{{item.type}}</td>
+                    <td>{{item.start_time}}</td>
+                    <td>{{item.end_time}}</td>
+                    <td>{{item.num}}</td>
+                    <td>
+                        <button class="operate_button">詳細資料</button>
+                    </td>
+                    <td>{{item.state}}</td>
+                    <td>
+                        <button v-if="index == 0" class="operate_button">取消申請</button> 
+                        <button v-else-if="index == 2" class="operate_button">歸還</button>
+                    </td>
+                </tr>
+                <tr class="empty-row">
+                    <td colspan="8"></td>
+                </tr>
+            </tbody>
+        </table>
+        </div>
+    </div>
+</template>
+
+<style>
+
+.inside_table{
+    border-radius: 25px; 
+    overflow: hidden;
+    width: 105%;
+    padding: 10px;
+    margin: 20px auto;
+    background-color: white;
+    position: relative;
+}
+
+table{
+    border-radius: 25px;
+    border:4px solid #ECE8E8; 
+    border-collapse: separate; 
+    border-spacing: 0;
+    overflow: hidden;
+    width: 97%;
+}
+
+tbody tr:last-child td {
+  padding-bottom: 50px; /* 你想要的空白高度 */
+}
+
+td,th{
+    border: none;
+    padding: 8px 12px;
+
+}
+
+tr th{
+    padding: 4px;
+}
+
+tr td{
+    text-align: center;
+    padding: 4px;
+}
+
+.head{
+    background-color: aliceblue
+}
+
+tbody td{
+    border-bottom: 2px solid #ECE8E8;
+}
+
+#data{
+    font-size: 20px;
+    color: black;
+    border-spacing: 0px;
+}
+
+.bottom_space{
+    height: 200px;
+}
+
+.operate_button{
+    border: none;
+    border-color:black;
+    background-color: #ECE8E8;
+    border-radius: 20px;
+    padding: 6px;
+}
+
+td{
+    font-size: 16px;
+    color: black;
+}
+
+.empty-row td {
+  height: 200px; 
+  border: none;
+  background: transparent;
+}
+
+</style>
--- src/views/RecordView.vue
+++ src/views/RecordView.vue
@@ -1,15 +1,66 @@
+<script setup>
+import Record_table from '../components/Record_table.vue';
+
+</script>
+
 <template>
-  <div class="title">
-    <h1>申請紀錄頁面</h1>
-  </div>
+  <h1 class="record">申請紀錄</h1>
+  <Record_table />
 </template>
 
 <style scoped>
-@media (min-width: 1024px) {
-  .title {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
+.text{
+  font-size: 35px;
+  color: black;
+  position: absolute;
+  top: 16px;
+  left: 580px;
 }
+.navbar span{
+  color: black;
+  font-weight: bold;
+  font-size: 23px;
+}
+.triangle-down{
+  width: 0;
+  height: 0;
+  margin-top: 12px;
+  font-size: 23px;
+  border-left: 5px solid transparent;
+  border-right: 5px solid transparent;
+  border-top: 6px solid black;
+  top: 10px;
+  cursor: pointer;
+}
+#first_text{
+  padding-left: 120px;
+}
+#last_text{
+  display: inline-flex;
+  gap: 4px;
+  color: black;
+  font-weight: bold;
+  padding-right: 50px;
+  font-size: 20px;
+  cursor: pointer; /*讓滑鼠變手指 使用者體驗更佳*/
+}
+.navbar{
+  position: absolute;
+  top: 110px;
+  left: 0%;
+  width: 100vw;
+  display: flex;
+  padding:10px 0;
+  justify-content: space-between;
+  background-color: aliceblue;
+  gap: 10px;
+}
+.record{
+  font-size: 30px;
+  color: black;
+  position: absolute;
+  top: 375px;
+  left: 55px;
+}
+
 </style>
