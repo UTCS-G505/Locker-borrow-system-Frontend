8b117e992a15b59c4dfad8b7a69293643378eded
Weining <ninghong0213@gmail.com>
2025-08-09 19:24:56 +0800

    fix:調整list項目排版
    華華要求要跟他的排版一樣，改成兩兩同一行

--- src/components/InfoPopup.vue
+++ src/components/InfoPopup.vue
@@ -17,50 +17,53 @@ const close = () => {
 
   <div v-if="detailInfo" class="overlay" @click.self="close">
     <div class="detailInfo">
-      <h2 class="title">詳細資訊</h2>
-      <div class="divider"></div>
+        <h2 class="title">詳細資訊</h2>
+        <div class="divider"></div>
+      
 
       <div class="info-list">
-        <div class="row">
-          <span class="label">申請人：</span>
-          <span class="value">陳胤華</span>
+      <div class="row">
+        <div class="field">
+          <span >申請人：</span>
+          <span >陳胤華</span>
         </div>
-        <div class="row">
-          <span class="label">狀態：</span>
-          <span class="value">借用中</span>
+        <div class="field">
+          <span >狀態：</span>
+          <span >借用中</span>
         </div>
-        <div class="row">
-          <span class="label">借用類別：</span>
-          <span class="value">學期借用</span>
+        <div class="field">
+          <span >借用類別：</span>
+          <span >學期借用</span>
         </div>
-        <div class="row">
-          <span class="label">申請時間：</span>
-          <span class="value">2025/6/30 9:50</span>
+        <div class="field">
+          <span >申請時間：</span>
+          <span >2025/6/30 9:50</span>
         </div>
-        <div class="row">
-          <span class="label">時間起：</span>
-          <span class="value">2024/9/1</span>
+        <div class="field">
+          <span >時間起：</span>
+          <span >2024/9/1</span>
         </div>
-        <div class="row">
-          <span class="label">時間迄：</span>
-          <span class="value">2025/6/30</span>
+        <div class="field">
+          <span >時間迄：</span>
+          <span >2025/6/30</span>
         </div>
-        <div class="row">
-          <span class="label">借用原因：</span>
-          <span class="value">沒有宿舍 QAQ</span>
+        <div class="field">
+          <span >借用原因：</span>
+          <span >沒有宿舍 QAQ</span>
         </div>
-        <div class="row">
-          <span class="label">駁回原因：</span>
-          <span class="value"></span>
+        <div class="field">
+          <span >駁回原因：</span>
+          <span ></span>
         </div>
-        <div class="row">
-          <span class="label">助教簽核時間：</span>
-          <span class="value"></span>
+        <div class="field">
+          <span >助教簽核時間：</span>
+          <span ></span>
         </div>
-        <div class="row">
-          <span class="label">系主任簽核時間：</span>
-          <span class="value"></span>
+        <div class="field">
+          <span >系主任簽核時間：</span>
+          <span ></span>
         </div>
+      </div>
       </div>
 
       
@@ -95,11 +98,30 @@ const close = () => {
 
 /* 彈窗本體 */
 .detailInfo {
-  background: white;
-  width: 400px;
+  background-color: white;
+  width: 700px;
   border-radius: 12px;
   padding: 20px;
   box-shadow: 0 4px 12px rgba(0, 0, 0, 0.2);
+  position: relative;
+  overflow: hidden;
+}
+
+.detailInfo::before{
+  content: "";
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 65px; 
+  background-color: #EBF7FFCC;
+  z-index: 0;
+}
+
+.detailInfo *{
+  position: relative;
+  z-index: 1;
+  background-color: transparent;
 }
 
 /* 標題 */
@@ -108,6 +130,8 @@ const close = () => {
   font-weight: bold;
   text-align: center;
   margin-bottom: 10px;
+  border-radius: 12px;
+  color: black;
 }
 
 /* 分隔線 */
@@ -123,25 +147,15 @@ const close = () => {
   flex-direction: column;
   gap: 8px;
 }
-
 .row {
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  gap: 8px 40px; 
+}
+
+.row .field {
   display: flex;
   align-items: center;
-  font-size: 14px;
-}
-
-
-
-.label {
-  color: #444;
-  width: 150px;          
-  text-align: left; 
-}
-
-.value {
-  color: #111;
-  flex: 1;             
-  text-align: left;  
 }
 
 /* 底部 */
