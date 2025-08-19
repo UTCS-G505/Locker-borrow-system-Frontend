8cffc2af05b9dff7e9211ff7745b34ac8ede7a00
Roy-1011 <aa0982822855@gmail.com>
2025-08-19 18:06:48 +0800

    revert App.vue, AppNavBar.vue and HomeView.vue to main

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,23 @@
 <script setup>
 import { RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
 import AppNavBar from './components/AppNavBar.vue';
 import AppFooter from './components/AppFooter.vue'
 </script>
 
 <template>
   <header>
+    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+    </div>
 
     <nav>
       <AppNavBar />
     </nav>
   </header>
-
+  
   <main>
     <RouterView />
   </main>
@@ -24,9 +30,15 @@ import AppFooter from './components/AppFooter.vue'
 <style scoped>
 header {
   line-height: 1.5;
+  min-height: 10vh;
   max-height: 100vh;
   margin: 0 auto;
-  padding: 0;
+  padding: 2rem;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
 }
 
 nav {
