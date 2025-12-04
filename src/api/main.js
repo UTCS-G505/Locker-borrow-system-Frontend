5659cedc94fa2d37f758839bf040ed300785ecb0
大空スバル大好き <111629702+BlueBoy247@users.noreply.github.com>
2025-12-04 18:26:11 +0800

    feat: 公告相關 API 請求 func

---
+++ src/api/main.js
@@ -0,0 +1,70 @@
+import apiMainV1 from "@/api/base";
+
+class Announcement {
+  static getGet = async (announcementId) => {
+    try {
+      const response = await apiMainV1.get(
+        `/announcement/get/${announcementId}`
+      );
+      return response.data.data;
+    } catch (err) {
+      console.error("獲取公告失敗", err);
+      return null;
+    }
+  };
+
+  static getAll = async (params) => {
+    try {
+      const response = await apiMainV1.get(
+        "/announcement/all",
+        { params }
+      );
+      return response.data.data;
+    } catch (err) {
+      console.error("獲取公告失敗", err);
+      return null;
+    }
+  };
+
+  static postCreate = async (data) => {
+    try {
+      const response = await apiMainV1.post(
+        "/announcement/create",
+        data
+      );
+      return response.data.data;
+    } catch (err) {
+      console.error("新增公告失敗", err);
+      return null;
+    }
+  }
+
+  static patchUpdate = async (announcementId, data) => {
+    try {
+      const response = await apiMainV1.patch(
+        `/announcement/update/${announcementId}`,
+        data
+      );
+      return response.data.data;
+    } catch (err) {
+      console.error("更新公告失敗", err);
+      return null;
+    }
+  }
+
+  static deleteDelete = async (announcementId) => {
+    try {
+      const response = await apiMainV1.delete(
+        `/announcement/delete/${announcementId}`
+      );
+      return response.data.data;
+    } catch (err) {
+      console.error("刪除公告失敗", err);
+      return null;
+    }
+  }
+}
+
+export { 
+  Announcement,
+}
