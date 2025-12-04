b58932009d73fffc657cd727d7cc296b955eaf46
大空スバル大好き <111629702+BlueBoy247@users.noreply.github.com>
2025-12-04 18:28:54 +0800

    feat: add pinia and state management

--- src/main.js
+++ src/main.js
@@ -1,11 +1,17 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
+import { useAuthStore } from './stores/auth'
 
 const app = createApp(App)
 
 app.use(router)
+app.use(createPinia())
+
+const authStore = useAuthStore()
+authStore.initAuth()
 
 app.mount('#app')
