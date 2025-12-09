824a39fc59ae6e3fc5456cda249eae0571927f56
AllenChang <111629702+BlueBoy247@users.noreply.github.com>
2025-12-10 03:53:05 +0800

    feat: sso login api

--- src/api/sso.js
+++ src/api/sso.js
@@ -1,6 +1,24 @@
 import { apiSsoV1 } from "@/api/base";
 
 class SsoAuth {
+  static postLogin = async (credentials) => {
+    try {
+      const response = await apiSsoV1.post(
+        `/auth/login`,
+        { credentials },
+        {
+          headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+          }
+        }
+      );
+      return response.data.data;
+    } catch (err) {
+      console.error("登入失敗", err);
+      return null;
+    }
+  }
+
   static postLogout = async () => {
     try {
       const response = await apiSsoV1.post(`/auth/logout`);
