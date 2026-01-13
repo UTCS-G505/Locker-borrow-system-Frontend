26b36014aa03c10d3feae1b16879a04862792c5d
4hshany <hanah950429@gmail.com>
2026-01-13 15:20:29 +0800

    實作身分註記列表學號和姓名欄位更新

--- src/components/NoteBoard.vue
+++ src/components/NoteBoard.vue
@@ -23,43 +23,14 @@ onMounted(async () => {
   if (data) {
     studentsList.value = data.map(user => ({
       id: user.id,
-      name: user.name || '使用者',
+      name: user.id,
+      
       // 狀態轉換邏輯
       note: user.state === 1 ? '住宿生註記' : 
-        user.state === 2 ? '違規註記' : null
+            user.state === 2 ? '違規註記' : null
     }));
   }
 });
-
-/* sample data
-const studentsList = ref([
-  { id: 'U11016099', name: '學生一', note: '住宿生註記' },
-  { id: 'U11116099', name: '學生二', note: null },
-  { id: 'U11216099', name: '學生三', note: '違規註記' },
-  { id: 'U11316099', name: '學生四', note: null },
-  { id: 'U11416099', name: 'Student Five', note: null },
-  { id: 'G11016099', name: '學生六', note: '住宿生註記' },
-  { id: 'G11116099', name: '學生七', note: null },
-  { id: 'G11216099', name: '學生八', note: '違規註記' },
-  { id: 'G11316099', name: '學生九', note: null },
-  { id: 'G11416099', name: '學生十', note: null },
-  { id: 'M11016099', name: '學生十一', note: '住宿生註記' },
-  { id: 'M11116099', name: '學生十二', note: null },
-  { id: 'M11216099', name: 'Student Thirteen', note: null },
-  { id: 'M11316099', name: '學生十四', note: null },
-  { id: 'M11416099', name: '學生十五', note: null },
-  { id: 'U11316100', name: '學生十六', note: '住宿生註記' },
-  { id: 'U11316101', name: '學生十七', note: '違規註記' },
-  { id: 'U11316102', name: '學生十八', note: '住宿生註記' },
-  { id: 'U11316103', name: '學生十九', note: null },
-  { id: 'U11316104', name: '學生二十', note: null },
-  { id: 'U11316105', name: 'Student Twenty First', note: null },
-  { id: 'U11316106', name: 'Student Twenty Second', note: null },
-  { id: 'U11316107', name: 'Student Twenty Third', note: '違規註記' },
-  { id: 'U11316108', name: 'Student Twenty Fourth', note: null },
-  { id: 'U11316109', name: 'Student Twenty Fifth', note: null },
-]);
-*/
 
 const filteredStudents = computed(() => {
   return studentsList.value.filter(student =>
