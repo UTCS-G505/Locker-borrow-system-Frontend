bfcad4aabfa1590b1bab4db408b2bb1877704414
morris950318 <162871887+morris950318@users.noreply.github.com>
2026-01-14 23:12:48 +0800

    fix: remove unused code from main merging

--- src/components/NoteBoard.vue
+++ src/components/NoteBoard.vue
@@ -4,7 +4,6 @@ import { User } from '@/api/main.js';
 import { ref, computed } from 'vue'
 import IconSearch from './icons/IconSearch.vue';
 import PopupViolationNote from './popups/PopupViolationNote.vue';
-import { User } from '@/api/main';
 import CheckPopup from './popups/CheckPopup.vue';
 
 const searchValue = ref('');
@@ -16,34 +15,6 @@ const title = ref('');
 // Note ID
 const USER_STATE = { NONE: 0, DORM: 1, VIOLATION: 2 };
 
-// sample data
-const studentsList = ref([
-  { id: 'U11016099', name: '學生一', note: '住宿生註記' },
-  { id: 'U11116099', name: '學生二', note: null },
-  { id: 'U11216099', name: '學生三', note: '違規註記' },
-  { id: 'U11316099', name: '學生四', note: null },
-  { id: 'U11416099', name: 'Student Five', note: null },
-  { id: 'G11016099', name: '學生六', note: '住宿生註記' },
-  { id: 'G11116099', name: '學生七', note: null },
-  { id: 'G11216099', name: '學生八', note: '違規註記' },
-  { id: 'G11316099', name: '學生九', note: null },
-  { id: 'G11416099', name: '學生十', note: null },
-  { id: 'M11016099', name: '學生十一', note: '住宿生註記' },
-  { id: 'M11116099', name: '學生十二', note: null },
-  { id: 'M11216099', name: 'Student Thirteen', note: null },
-  { id: 'M11316099', name: '學生十四', note: null },
-  { id: 'M11416099', name: '學生十五', note: null },
-  { id: 'U11316100', name: '學生十六', note: '住宿生註記' },
-  { id: 'U11316101', name: '學生十七', note: '違規註記' },
-  { id: 'U11316102', name: '學生十八', note: '住宿生註記' },
-  { id: 'U11316103', name: '學生十九', note: null },
-  { id: 'U11316104', name: '學生二十', note: null },
-  { id: 'U11316105', name: 'Student Twenty First', note: null },
-  { id: 'U11316106', name: 'Student Twenty Second', note: null },
-  { id: 'U11316107', name: 'Student Twenty Third', note: '違規註記' },
-  { id: 'U11316108', name: 'Student Twenty Fourth', note: null },
-  { id: 'U11316109', name: 'Student Twenty Fifth', note: null },
-]);
 // 空的響應式陣列，用來接 API 資料
 const studentsList = ref([]);
 
