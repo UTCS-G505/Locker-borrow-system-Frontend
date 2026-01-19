da8b5a5275682ec9bc166b89dd9ad1c72522763c
benjamin666520 <benjamin950428666@gmail.com>
2026-01-19 20:18:38 +0800

    fix:還原main.js

--- src/api/main.js
+++ src/api/main.js
@@ -73,7 +73,7 @@ class User {
 
     } catch (err) {
       console.error("取得使用者資料失敗", err);
-      return null; 
+      return null;
     }
   };
 
@@ -134,7 +134,7 @@ class Record {
       return [];
     }
   };
-  
+
   static postBorrow = async (data) => {
     try{
       const response = await apiMainV1.post('/record/borrow', data);
@@ -164,19 +164,7 @@ class Record {
 
   static postReturn;
 
-  static postReviewReturn = async (recordId, params) => {
-    try {
-      const response = await apiMainV1.post(
-        `/record/reviewr/${recordId}`,
-        null,
-        { params }
-      );
-      return response.data.data;
-    } catch (err) {
-      console.error("審核歸還失敗", err);
-      return null;
-    }
-  };
+  static postReviewReturn;
 }
 
 class Locker {
